215cb3990cd6255a74b0e0dc29bbc14b32aaf2ad
udion <uddeshya.upa@gmail.com>
2017-05-17 23:05:40 +0800

    decryptor added

--- src/PRESENT-code.jl
+++ src/PRESENT-code.jl
@@ -29,8 +29,8 @@ function PRESENTenc(K, inp)
         round_key = keys[i]
         state = xor(state,keys[i])
         state = sLayer(state)
-        round_outp = state
         state = pLayer(state)
+        round_outp = state
 
         rounds_data[i] = roundData(round_inp,round_key,round_outp)
     end
@@ -40,9 +40,46 @@ function PRESENTenc(K, inp)
     round_outp = state
     rounds_data[32] = roundData(round_inp,round_key,round_outp)
 
-    rounds_data
-
     #to output the ciphertext
+    bin2hex(rounds_data[32].outp,16)
+end
+
+#PRESENTdec will be the actual function which will
+#be decrypting initial state (testvector)
+#PRESENTdec will be a wrapper around the decrypting algorithm.
+#each rounds data will be stored in roundData type
+#the input key and the state is expected in the hexadecimal
+#hexadecimal starting WITHOUT "0x"
+
+#decryption will follow the following scheme of operations
+#xorKey        |
+#invpLayer     | ---> this happens 31 times
+#invsLayer     |
+#xorKey
+function PRESENTdec(K, inp)
+    rounds = 31
+    k_seed  = hex2bin(K,80)
+    keys = genKeys(k_seed)
+    state = hex2bin(inp,64) #this represents the ciphertext
+    rounds_data = fill(undef_round,32)
+
+    for i in 1:rounds
+        round_inp = state
+        round_key = keys[33-i]
+        state = xor(state,keys[33-i])
+        state = invpLayer(state)
+        state = invsLayer(state)
+        round_outp = state
+
+        rounds_data[i] = roundData(round_inp,round_key,round_outp)
+    end
+    round_inp = state
+    round_key = keys[33-32]
+    state = xor(state,keys[33-32])
+    round_outp = state
+    rounds_data[32] = roundData(round_inp,round_key,round_outp)
+
+    #to output the plaintext
     bin2hex(rounds_data[32].outp,16)
 end
 #########################################################################
